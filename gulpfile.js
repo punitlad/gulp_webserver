df5fed4ca22610b0a4b380c0dc47b967400495bc
Punit Lad <plad@thoughtworks.com>
2015-03-17 15:07:34 -0400

    added gulp mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var mocha = require('gulp-mocha');
+ 
+gulp.task('tests', function () {
+    return gulp.src('test/test.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
+});
 
 gulp.task('webserver', function() {
     connect.server({
